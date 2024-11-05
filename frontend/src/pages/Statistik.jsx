87a71f09094973f116aecdec8db4312850611f42
Klimperklaus <steven.mueller@dci-student.org>
2024-11-05 16:25:06 +0100

    hoffen und beten

--- frontend/src/pages/Statistik.jsx
+++ frontend/src/pages/Statistik.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import banner from "../img/banner.png";
-import { Link } from "react-router-dom";
+
 function UserStats() {
   const [username, setUsername] = useState("");
   const [clicks, setClicks] = useState(0);
   const [tier, setTier] = useState(1);
   const [timer, setTimer] = useState(10000);
-  const  [totalEntries, setTotalEntries] = useState(0);
+  const [totalEntries, setTotalEntries] = useState(0);
   useEffect(() => {
     const fetchUserStats = async () => {
       const token = Cookies.get("token_js");
@@ -59,17 +59,28 @@ function UserStats() {
   }, []);
 
   return (
-    <div className="user-stats">
-      <h1>Hey {username}!</h1>
-      <div className="data">Alle Pixel: <span>{totalEntries}</span></div>
-      <div className="data">Anzahl deiner Pixel: <span>{clicks}</span></div>
-      {/* <div>Dein Anteil am Ganzen: {percentage}%</div> */}
-      <div className="data">Deine Stufe: <span>{tier}</span> von 4</div>
-      <div className="data">Dein Timeout: <span>{timer}</span>ms</div>
-      <div><img src={banner} alt="banner" /></div>
+    <div className="wrapper">
+      <div className="user-stats">
+        <h1>Hey {username}!</h1>
+        <div className="data">
+          Alle Pixel: <span>{totalEntries}</span>
+        </div>
+        <div className="data">
+          Anzahl deiner Pixel: <span>{clicks}</span>
+        </div>
+        {/* <div>Dein Anteil am Ganzen: {percentage}%</div> */}
+        <div className="data">
+          Deine Stufe: <span>{tier}</span> von 4
+        </div>
+        <div className="data">
+          Dein Timeout: <span>{timer}</span>ms
+        </div>
+        <div>
+          <img src={banner} alt="banner" />
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default UserStats;
