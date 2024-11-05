5757938fdd9e2e17726824e5b761f440972268d0
DerTimStegemann <DerTimStegemann@gmail.com>
2024-11-05 14:59:16 +0100

    Styleänderungen

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,21 +1,13 @@
 import { Outlet } from "react-router-dom";
 // import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Header from "./Header";
 
 function Layout() {
   return (
     <>
-      <header>
-        <Header/>
-      </header>
-       {/* <Navbar/> */}
+      {/* <Navbar/> */}
       <main>
         <Outlet />
       </main>
-      <footer>
-        <Footer />
-      </footer>
     </>
   );
 }
