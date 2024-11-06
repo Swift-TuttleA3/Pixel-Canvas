339b314965c8375a674fa4afeeaabd8bf32c8760
Klimperklaus <steven.mueller@dci-student.org>
2024-11-06 17:40:03 +0100

    stats workaround, parallax need to be implemented

--- frontend/src/pages/Statistik.jsx
+++ frontend/src/pages/Statistik.jsx
@@ -9,7 +9,7 @@ function UserStats() {
   const [clicks, setClicks] = useState(0);
   const [tier, setTier] = useState(1);
   const [timer, setTimer] = useState(10000);
-  const  [totalEntries, setTotalEntries] = useState(0);
+  const [totalEntries, setTotalEntries] = useState(0);
   useEffect(() => {
     const fetchUserStats = async () => {
       const token = Cookies.get("token_js");
@@ -63,24 +63,40 @@ function UserStats() {
   const percentage = ((clicks / totalEntries) * 100).toFixed(2);
   return (
     <>
-    <NavbarBurger/>
-    <Navbar/>
-    <div className="user-stats p-6 bg-gray-800 text-white rounded-lg">
-      <h1 className="text-3xl mb-4 pixel-font text-yellow-500">Hey {username}!</h1>
-      <div className="data mb-4">Dein Timeout: <span>{timer / 1000}</span> Sekunden</div>
-      <div className="data mb-2">Deine Stufe: <span>{tier}</span> von 4</div>
-      <div className="data mb-2">Alle Pixel: <span>{totalEntries}</span></div>
-      <div className="data mb-2">Anzahl deiner Pixel: <span>{clicks}</span></div>
-      <div className="data mb-2">Dein Anteil am Ganzen:</div>
-      <div className="w-full bg-gray-700 rounded-md h-6 mb-2">
-        <div
-          className="bg-customTertiary h-full rounded-md"
-          style={{ width: `${percentage}%` }}
-          >{percentage}%</div>
+      <NavbarBurger />
+      <Navbar />
+      <div className="wrapper">
+        <div className="user-stats p-6 bg-gray-800 text-white rounded-lg">
+          <h1 className="text-3xl mb-4 pixel-font text-yellow-500">
+            Hey {username}!
+          </h1>
+          <div className="data mb-4">
+            Dein Timeout: <span>{timer / 1000}</span> Sekunden
+          </div>
+          <div className="data mb-2">
+            Deine Stufe: <span>{tier}</span> von 4
+          </div>
+          <div className="data mb-2">
+            Alle Pixel: <span>{totalEntries}</span>
+          </div>
+          <div className="data mb-2">
+            Anzahl deiner Pixel: <span>{clicks}</span>
+          </div>
+          <div className="data mb-2">Dein Anteil am Ganzen:</div>
+          <div className="w-full bg-gray-700 rounded-md h-6 mb-2">
+            <div
+              className="bg-customTertiary h-full rounded-md"
+              style={{ width: `${percentage}%` }}
+            >
+              {percentage}%
+            </div>
+          </div>
+          <div>
+            <img src={banner} alt="banner" className="rounded-md mt-4" />
+          </div>
+        </div>
       </div>
-      <div><img src={banner} alt="banner" className="rounded-md mt-4" /></div>
-    </div>
-          </>
+    </>
   );
 }
 
