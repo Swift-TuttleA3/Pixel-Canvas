d8c2653d29ca4c0381e7c6c8da04368c970a198b
Swift-TuttleA3 <r.panek888@gmail.com>
2024-11-06 13:09:20 +0100

    update teamPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
         <Route
           path="/devdesk"
           element={
-            <PrivateRoute>
+            <PrivateRoute adminOnly={true}>
               <DevDesk />
             </PrivateRoute>
           }
